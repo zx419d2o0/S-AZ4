e9bfad88ffc68397d2cedd4758969c409dd2d6eb
zx419d2o0 <869969681@qq.com>
2021-07-18 14:28:49 +0800

    test

---
+++ TM/comMatabattle.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         MATABATTLE CHS
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://metabattle.com/wiki/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @updateURL    https://zx419d2o0.github.io/S-AZ4/TM/comMatabattle.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    document.GW2A_EMBED_OPTIONS = {lang: 'zh'};
+})();
